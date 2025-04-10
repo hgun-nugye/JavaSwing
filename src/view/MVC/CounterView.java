4acbc1e993568530d6fa48ca4ef6874ae070a7b2
Huong Nguyen <thanhhuong10102005@gmail.com>
2025-04-10 19:04:24 +0700

    add button 'reset'

--- src/view/MVC/CounterView.java
+++ src/view/MVC/CounterView.java
@@ -11,6 +11,7 @@ public class CounterView extends JFrame {
     private CounterModel counterModel;
     private JButton jButton_up;
     private JButton jButton_down;
+    private JButton jButton_reset;
     private JLabel jLabel_value;
 
     public CounterView() {
@@ -33,14 +34,18 @@ public class CounterView extends JFrame {
         jButton_down = new JButton("DOWN");
         jButton_down.addActionListener(ac);
 
+        jButton_reset=new JButton("RESET");
+        jButton_reset.addActionListener(ac);
+
         jLabel_value = new JLabel(this.counterModel.getValue() + "", JLabel.CENTER);
 
         JPanel jPanel_center = new JPanel();
-        jPanel_center.setLayout(new BorderLayout());
+        jPanel_center.setLayout(new BorderLayout(10,10));
 
         jPanel_center.add(jButton_up, "West");
         jPanel_center.add(jLabel_value, "Center");
         jPanel_center.add(jButton_down, "East");
+        jPanel_center.add(jButton_reset, "South");
 
         this.setLayout(new BorderLayout());
         this.add(jPanel_center, "Center");
@@ -56,5 +61,10 @@ public class CounterView extends JFrame {
     {
         this.counterModel.decrement();
         this.jLabel_value.setText(this.counterModel.getValue()+"");
+    }
+
+    public void reset(){
+        this.counterModel.reset();
+        this.jLabel_value.setText("0");
     }
 }
